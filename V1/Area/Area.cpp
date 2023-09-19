6183ce629d4be76ff9493414dc70a977c2d492e7
M4rvinz <143666056+M4rvinz@users.noreply.github.com>
2023-09-18 18:36:45 -0600

    Add files via upload
    Area cuadrado

---
+++ V1/Area/Area.cpp
@@ -0,0 +1,15 @@
+
+
+
+#include <iostream>
+
+int main()
+{
+    int area,lado;
+    
+    printf("Lado: ");
+    scanf_s("%i", &lado);
+  
+    area = lado*lado;
+    printf("El area es de %i", area);
+}
