b1707d0a41963af1de3f93bb1d75d92b7b057554
M4rvinz <143666056+M4rvinz@users.noreply.github.com>
2023-09-18 18:44:03 -0600

    Add files via upload
    Area de un circulo

---
+++ V3/Area/Area.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include <iostream>
+
+int main()
+{
+    int area,radio;
+    
+    printf("Radio: ");
+    scanf_s("%i", &radio);
+  
+    area = 3.1416*radio*radio;
+
+    printf("El area es de %i", area);
+}
