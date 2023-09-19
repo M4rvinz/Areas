d2e4288f9fbbc43daaa30b74064f06b70da4d570
M4rvinz <143666056+M4rvinz@users.noreply.github.com>
2023-09-18 18:39:50 -0600

    Add files via upload
    Area de un rectangulo

---
+++ V2/Area/Area.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include <iostream>
+
+int main()
+{
+    int area,base,altura;
+    
+    printf("Base: ");
+    scanf_s("%i", &base);
+    printf("Altura: ");
+    scanf_s("%i", &altura);
+  
+    area = base*altura;
+    printf("El area es de %i", area);
+}
